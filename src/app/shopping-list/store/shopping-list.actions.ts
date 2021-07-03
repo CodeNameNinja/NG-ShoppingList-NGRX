f1685eecaf3a8c511823ca22f0915f115f4d2f6b
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-07-03 19:59:36 +0200

    feat: implementing shopping list update and delete in store

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,23 +1,36 @@
 import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENT = "ADD_INGREDIENT";
 export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
+export const UPDATE_INGREDIENT = "UPDATE_INGREDIENT";
+export const DELETE_INGREDIENT = "DELETE_INGREDIENT";
 
-export class AddIngredient implements Action{
-    readonly type: string  =  ADD_INGREDIENT;
-    
-    constructor(public payload:Ingredient){
-    }
+
+export class AddIngredient implements Action {
+  readonly type: string = ADD_INGREDIENT;
+
+  constructor(public payload: Ingredient) {}
 }
-export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+export const REMOVE_INGREDIENT = "REMOVE_INGREDIENT";
 
-export class AddIngredients implements Action{
-    readonly type: string = ADD_INGREDIENTS;
+export class AddIngredients implements Action {
+  readonly type: string = ADD_INGREDIENTS;
 
-    constructor(public payload:Ingredient[]){
-
-    }
+  constructor(public payload: Ingredient[]) {}
+}
+export class UpdateIngredient implements Action {
+  readonly type: string = UPDATE_INGREDIENT;
+  constructor(public payload: { index: number, ingredient: Ingredient }) {}
 }
 
-export type ShoppingListActions = AddIngredients | AddIngredient
+export class DeleteIngredient implements Action {
+  readonly type: string = DELETE_INGREDIENT;
+  constructor(public payload: number) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredients
+  | AddIngredient
+  | UpdateIngredient
+  | DeleteIngredient;
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,22 +1,51 @@
 import { Ingredient } from "../../shared/ingredient.model";
-import  * as ShoppingListActions  from './shopping-list.actions';
+import * as ShoppingListActions from "./shopping-list.actions";
 const initialState = {
-    ingredients: [
-        new Ingredient("Apples", 5), 
-        new Ingredient("Tomatoes", 10)
-    ],
+  ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)],
 };
 
-export function shoppingListReducer(state = initialState, action:ShoppingListActions.ShoppingListActions) {
-    switch(action.type){
-        case ShoppingListActions.ADD_INGREDIENT:
-            return {...state, ingredients: [...state.ingredients, action.payload]}
-        case ShoppingListActions.ADD_INGREDIENTS:
-            // @ts-ignore
-            return {...state, ingredients: [...state.ingredients, ...action.payload]}
-        case ShoppingListActions.REMOVE_INGREDIENT:
-            // return {...state, ingredients: [state.ingredients.splice(action.payload, 1)]};
-        default: 
-            return state;
-    }
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.ShoppingListActions
+) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return { ...state, ingredients: [...state.ingredients, action.payload] };
+
+
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        // @ts-ignore
+        ingredients: [...state.ingredients, ...action.payload],
+      };
+
+
+    case ShoppingListActions.UPDATE_INGREDIENT:
+        console.log(action.payload);
+      // @ts-ignore
+      const ingredient = state.ingredients[action.payload.index];
+      //   @ts-ignore
+      const updateIngredient = { ...ingredient, ...action.payload.ingredient };
+      const updateIngredients = [...state.ingredients];
+      //   @ts-ignore
+      updateIngredients[action.payload.index] = updateIngredient;
+      return {
+        ...state,
+        ingredients: updateIngredients,
+      };
+
+
+      
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ingredient, index) => {
+          //   @ts-ignore
+          return index !== action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
 }
